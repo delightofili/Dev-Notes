f9abd4259cf2a69e3ad0d91d8e2c2b2f79d68c31
Delightsome <delightofili0@gmail.com>
2026-05-16 20:03:49 +0100

    created header and nav

--- components/main-header.js
+++ components/main-header.js
@@ -1,25 +1,24 @@
 import Link from "next/link";
 import { IoLogoDeviantart } from "react-icons/io5";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
-    <div className=" flex items-center justify-between py-4 px-6 ">
+    <div className=" flex items-center justify-between py-6 px-8 ">
       <div className="flex items-center">
         <IoLogoDeviantart className="text-green-500 text-4xl" />
-        <h1 className="text-white text-3xl font-bold">
+        <Link href="/" className="text-white text-3xl font-bold">
           Dev<span className="text-green-500">Notes</span>
-        </h1>
+        </Link>
       </div>
-      <div>
+      <div className="mr-7">
         <nav>
-          <ul>
+          <ul className="flex gap-6">
             <li>
-              <Link
-                href="/notes"
-                className="text-gray-300 hover:text-white transition-colors duration-300"
-              >
-                Notes
-              </Link>
+              <NavLink href="/notes">Notes</NavLink>
+            </li>
+            <li>
+              <NavLink href="/notes/share">Share</NavLink>
             </li>
           </ul>
         </nav>
